3c931f1b0c5984384218f8f9fdf3dcb3a9251ddb
Gorbkos <gniewkoscielak@gmail.com>
2023-11-29 09:56:37 +0100

    tests

--- test.py
+++ test.py
@@ -15,3 +15,14 @@ def test_get_user(client):
     response = client.get('/users/1')
     assert response.status_code == 200
     assert json.loads(response.get_data()) == {'id': 1, 'name': 'Gniewko', 'lastname': 'Koscielak'}
+
+
+def test_get_user_not_found(client):
+    response = client.get('/users/100')
+    assert response.status_code == 404
+    assert json.loads(response.get_data()) == {'error': 'User not found'}
+
+def test_create_user(client):
+    response = client.post('/users', json={'name': 'Jan', 'lastname': 'Kowalski'})
+    assert response.status_code == 201
+    assert json.loads(response.get_data()) == {'id': 7}
