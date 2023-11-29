df05beef15f744af579225f0aaed099b99c7d6bd
Gorbkos <gniewkoscielak@gmail.com>
2023-11-29 10:01:38 +0100

    tests

--- test.py
+++ test.py
@@ -26,3 +26,18 @@ def test_create_user(client):
     response = client.post('/users', json={'name': 'Jan', 'lastname': 'Kowalski'})
     assert response.status_code == 201
     assert json.loads(response.get_data()) == {'id': 7}
+
+def test_create_user_invalid_body(client):
+    response = client.post('/users', json={'name': 'Jan'})
+    assert response.status_code == 400
+    assert json.loads(response.get_data()) == {'error': 'Invalid request body'}
+
+def test_update_user(client):
+    response = client.patch('/users/1', json={'name': 'Jan', 'lastname': 'Kowalski'})
+    assert response.status_code == 204
+    assert response.data == b''
+
+def test_update_user_not_found(client):
+    response = client.patch('/users/100', json={'name': 'Jan', 'lastname': 'Kowalski'})
+    assert response.status_code == 400
+    assert json.loads(response.get_data()) == {'error': 'User not found'}
